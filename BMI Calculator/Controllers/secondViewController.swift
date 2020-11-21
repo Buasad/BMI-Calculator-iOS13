f4ba68b396c4a05a2fa43f5519573ffa304811a1
Buasad <hamoodat@live.com>
2020-11-21 13:39:27 +0300

    ;

---
+++ BMI Calculator/Controllers/secondViewController.swift
@@ -0,0 +1,15 @@
+//
+//  secondViewController.swift
+//  BMI Calculator
+//
+//  Created by Mohammed Alasad on 11/21/20.
+//  Copyright © 2020 Angela Yu. All rights reserved.
+//
+import UIKit
+
+class SecondViewController: UIViewController {
+    
+    override func viewDidLoad()
+    super.viewDidLoad()
+    
+}
